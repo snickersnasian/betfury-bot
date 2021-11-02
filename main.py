81ebd3771ac5c05356874fa54746bbc7f39d2c25
zaiden54 <zaiden.ranepa@gmail.com>
2021-11-02 13:43:51 +0300

    added betting system, divided code into modules, beta-version 1.0

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as ec
 from fake_useragent import UserAgent
-import credentials
 import time
-from selenium.common.exceptions import NoSuchElementException
+
+from adblocker import adblocker
+from login import authenticate
+from find_streaks import find_streaks
+from launch_game import launch_games
 
 ua = UserAgent()
 
@@ -21,7 +21,6 @@ options.add_argument("--ignore-ssl-errors")
 # disable webdriver-mode
 options.add_argument("--disable-blink-features=AutomationControlled")
 
-
 def main():
 
     try:
@@ -35,130 +34,16 @@ def main():
 
         time.sleep(5)
 
-        try: #pop-up detect and close
-            WebDriverWait(driver, 5).until(
-                ec.visibility_of_element_located((By.CSS_SELECTOR, ".new-popup__container")))
-            driver.find_element_by_css_selector('button.new-popup__btn-close').click()
-        except:
-            print('No pop-up found')
+        adblocker(driver)
 
-        # authenticate
-        WebDriverWait(driver, 200).until(ec.visibility_of_element_located(
-            (By.CSS_SELECTOR, ".btn.btn_outline.btn_medium")))
-        driver.find_element_by_css_selector(
-            '.btn.btn_outline.btn_medium').click()
+        authenticate(driver)
 
-        WebDriverWait(driver, 200).until(
-            ec.visibility_of_element_located((By.CSS_SELECTOR, ".input__value")))
-        loginInput = driver.find_elements_by_css_selector('.input__value')[0]
-        loginInput.clear()
-        loginInput.send_keys(credentials.login)
-        time.sleep(2)
-
-        passInput = driver.find_elements_by_css_selector('.input__value')[1]
-        passInput.clear()
-        passInput.send_keys(credentials.password)
-
-        submit_button = driver.find_element_by_css_selector(
-            '.btn.btn_large.btn_block.btn_red')
-        submit_button.click()
-
-        # Set TRX currency
-        WebDriverWait(driver, 200).until(
-            ec.visibility_of_element_located((By.CSS_SELECTOR, ".balance__inner")))
-
-        driver.find_element_by_css_selector('.balance__inner').click()
-        driver.find_element_by_css_selector(
-            'div.balance-dropdown__inner.balance-dropdown__inner--darkened > ul > li:nth-child(4)').click()
-
-        # Start game
-        WebDriverWait(driver, 200).until(ec.visibility_of_element_located(
-            (By.CSS_SELECTOR, "#play_button")))
-        driver.find_element_by_css_selector('#play_button').click()
-
-        # Switch to game iframe
-        WebDriverWait(driver, 200).until(ec.visibility_of_element_located(
-            (By.CSS_SELECTOR, "div.single-game-modal-body-block iframe")))
-        driver.switch_to.frame(driver.find_element_by_css_selector(
-            "div.single-game-modal-body-block iframe"))
-
-        WebDriverWait(driver, 200).until(ec.visibility_of_element_located(
-            (By.CSS_SELECTOR, "div.multiplayButtonContainer--a_4PI > div > div > button")))
-        driver.find_element_by_css_selector(
-            "div.multiplayButtonContainer--a_4PI > div > div > button").click()
-
-        # Switch to game side-bar iframe
-        WebDriverWait(driver, 200).until(ec.visibility_of_element_located(
-            (By.CSS_SELECTOR, "div.sidebar-container > iframe")))
-        driver.switch_to.frame(driver.find_element_by_css_selector(
-            "div.sidebar-container > iframe"))
-
-        # Collect games
-        WebDriverWait(driver, 200).until(ec.visibility_of_element_located(
-            (By.CSS_SELECTOR, ".item--1TwGJ")))
-        games = driver.find_elements_by_css_selector(".item--1TwGJ")
-
-        WebDriverWait(driver, 200).until(ec.visibility_of_element_located(
-            (By.CSS_SELECTOR, "svg > svg > g")))
+        games = launch_games(driver)
         
         time.sleep(3)
 
-        def find_streaks():
-            try:
-                for game in games:
+        find_streaks(games, driver)
 
-                    streak = 0
-
-                    table_name = game.find_element_by_css_selector(
-                        'span.tableName--3PUPn')
-                    # print("\n" + table_name.text)
-
-                    last_dot = game.find_element_by_css_selector(
-                        '.item--1TwGJ div.roadContainer--2ujMr svg svg[data-type="coordinates"]:last-child')
-
-                    # print('X:' + last_dot.get_attribute("x"))
-                    # print('Y:' + last_dot.get_attribute("y"))
-
-                    x_coor = last_dot.get_attribute("x")
-                    y_coor = last_dot.get_attribute("y")
-
-                    if y_coor == "5":
-                        print("\n" + table_name.text)
-
-                        upper_dot = True
-
-                        while (upper_dot):
-                            try:
-                                game.find_element_by_css_selector (f'.item--1TwGJ div.roadContainer--2ujMr svg svg[data-type="coordinates"][x="{x_coor}"][y="{str(int(y_coor) - 1)}"]')
-                                upper_dot = False
-                            except:
-                                upper_dot = True
-                            
-                            streak += 1
-
-                            x_coor = str(int(x_coor) - 1)
-                        print(streak)
-                        print(last_dot.find_element_by_css_selector('g').get_attribute('fill'))
-
-                        # red  #EC2024
-                        # blue #2E83FF
-
-                        blueButton = game.find_element_by_css_selector('div.footerControls--3u5nU > div:nth-child(1) > div')
-
-                        redButton = game.find_element_by_css_selector('div.footerControls--3u5nU > div:nth-child(2) > div')
-
-
-                
-                find_streaks()
-
-            except:
-                time.sleep(5)
-                driver.find_element_by_css_selector('div.right--350jI > button').click()
-                find_streaks()
-
-        find_streaks()
-
-        
         time.sleep(10)
         driver.close()
 
