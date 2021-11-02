10af9d0aa1404867074645910f28d0e02922ae55
duyanh <duy-anh@mail.ru>
2021-11-02 11:22:32 +0300

    Add games refresh, fix streak counter, add color picker

--- main.py
+++ main.py
@@ -101,45 +101,64 @@ def main():
         WebDriverWait(driver, 200).until(ec.visibility_of_element_located(
             (By.CSS_SELECTOR, "svg > svg > g")))
         
-        time.sleep(5)
+        time.sleep(3)
 
-        for game in games:
+        def find_streaks():
+            try:
+                for game in games:
 
-            streak = 0
+                    streak = 0
 
-            table_name = game.find_element_by_css_selector(
-                'span.tableName--3PUPn')
-            # print("\n" + table_name.text)
+                    table_name = game.find_element_by_css_selector(
+                        'span.tableName--3PUPn')
+                    # print("\n" + table_name.text)
 
-            last_dot = game.find_element_by_css_selector(
-                '.item--1TwGJ div.roadContainer--2ujMr svg svg[data-type="coordinates"]:last-child')
+                    last_dot = game.find_element_by_css_selector(
+                        '.item--1TwGJ div.roadContainer--2ujMr svg svg[data-type="coordinates"]:last-child')
 
-            # print('X:' + last_dot.get_attribute("x"))
-            # print('Y:' + last_dot.get_attribute("y"))
+                    # print('X:' + last_dot.get_attribute("x"))
+                    # print('Y:' + last_dot.get_attribute("y"))
 
-            x_coor = last_dot.get_attribute("x")
-            y_coor = last_dot.get_attribute("y")
+                    x_coor = last_dot.get_attribute("x")
+                    y_coor = last_dot.get_attribute("y")
 
-            if y_coor == "5":
-                print("\n" + table_name.text)
+                    if y_coor == "5":
+                        print("\n" + table_name.text)
 
-                upper_dot = True
-
-                while (upper_dot):
-                    try:
-                        game.find_element_by_css_selector (f'.item--1TwGJ div.roadContainer--2ujMr svg svg[data-type="coordinates"][x="{x_coor}"][y="{str(int(y_coor) - 1)}"]')
-                        upper_dot = False
-                    except:
                         upper_dot = True
-                    
-                    streak += 1
 
-                    x_coor = str(int(x_coor) - 1)
-                print(streak)    
+                        while (upper_dot):
+                            try:
+                                game.find_element_by_css_selector (f'.item--1TwGJ div.roadContainer--2ujMr svg svg[data-type="coordinates"][x="{x_coor}"][y="{str(int(y_coor) - 1)}"]')
+                                upper_dot = False
+                            except:
+                                upper_dot = True
+                            
+                            streak += 1
+
+                            x_coor = str(int(x_coor) - 1)
+                        print(streak)
+                        print(last_dot.find_element_by_css_selector('g').get_attribute('fill'))
+
+                        # red  #EC2024
+                        # blue #2E83FF
+
+                        blueButton = game.find_element_by_css_selector('div.footerControls--3u5nU > div:nth-child(1) > div')
+
+                        redButton = game.find_element_by_css_selector('div.footerControls--3u5nU > div:nth-child(2) > div')
 
 
                 
+                find_streaks()
 
+            except:
+                time.sleep(5)
+                driver.find_element_by_css_selector('div.right--350jI > button').click()
+                find_streaks()
+
+        find_streaks()
+
+        
         time.sleep(10)
         driver.close()
 
